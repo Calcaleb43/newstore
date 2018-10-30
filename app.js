abec1f3f806a3270a97f71a612370f4f1194977c
Calcaleb43 <cal.caleb43@gmail.com>
2018-10-30 17:17:32 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,116 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const cors = require('cors')
+const PORT = process.env.PORT || 5000
+
+app.use(express.static(__dirname+'/client'));
+app.use(bodyParser.json());
+app.use(cors())
+app.get('/book/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+});
+
+Genre =require('./models/genre');
+Book =require('./models/book');
+
+// Connect to Mongoose
+mongoose.connect('mongodb://Calcaleb:Cal0505028700@ds213183.mlab.com:13183/cms');
+var db = mongoose.connection;
+
+app.get('/', (req, res) => {
+	res.send('Please use /api/books or /api/genres');
+});
+
+app.get('/api/genres', (req, res) => {
+	Genre.getGenres((err, genres) => {
+		if(err){
+			throw err;
+		}
+		res.json(genres);
+	});
+});
+
+app.post('/api/genres', (req, res) => {
+	var genre = req.body;
+	Genre.addGenre(genre, (err, genre) => {
+		if(err){
+			throw err;
+		}
+		res.json(genre);
+	});
+});
+
+app.put('/api/genres/:_id', (req, res) => {
+	var id = req.params._id;
+	var genre = req.body;
+	Genre.updateGenre(id, genre, {}, (err, genre) => {
+		if(err){
+			throw err;
+		}
+		res.json(genre);
+	});
+});
+
+app.delete('/api/genres/:_id', (req, res) => {
+	var id = req.params._id;
+	Genre.removeGenre(id, (err, genre) => {
+		if(err){
+			throw err;
+		}
+		res.json(genre);
+	});
+});
+
+app.get('/api/books', (req, res) => {
+	Book.getBooks((err, books) => {
+		if(err){
+			throw err;
+		}
+		res.json(books);
+	});
+});
+
+app.get('/api/books/:_id', (req, res) => {
+	Book.getBookById(req.params._id, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+app.post('/api/books', (req, res) => {
+	var book = req.body;
+	Book.addBook(book, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+app.put('/api/books/:_id', (req, res) => {
+	var id = req.params._id;
+	var book = req.body;
+	Book.updateBook(id, book, {}, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+app.delete('/api/books/:_id', (req, res) => {
+	var id = req.params._id;
+	Book.removeBook(id, (err, book) => {
+		if(err){
+			throw err;
+		}
+		res.json(book);
+	});
+});
+
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+console.log('Running on port 5000...');
